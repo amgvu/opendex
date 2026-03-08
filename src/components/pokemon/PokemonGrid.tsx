8b1455d56bfaa019dfb5106187b0b076f71bf108
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2026-03-07 23:03:02 -0600

    feat: pokemon url params

--- src/components/pokemon/PokemonGrid.tsx
+++ src/components/pokemon/PokemonGrid.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { useCallback, useState } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useCallback, useEffect, useState } from 'react'
 
 import type { Pokemon } from '@/types/pokemon'
 
@@ -15,7 +16,22 @@ export default function PokemonGrid() {
   const { debouncedSearch, search, setSearch } = useSearch()
   const { fetchNextPage, hasNextPage, isFetchingNextPage, pokemon, status } =
     usePokemon(debouncedSearch)
-  const [selectedId, setSelectedId] = useState<null | number>(null)
+
+  const searchParams = useSearchParams()
+  const router = useRouter()
+  const [selectedId, setSelectedId] = useState<null | number>(
+    searchParams.get('pokemon') ? Number(searchParams.get('pokemon')) : null
+  )
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams.toString())
+    if (selectedId !== null) {
+      params.set('pokemon', String(selectedId))
+    } else {
+      params.delete('pokemon')
+    }
+    router.replace(`?${params.toString()}`, { scroll: false })
+  }, [selectedId]) // eslint-disable-line react-hooks/exhaustive-deps
 
   const onLoadMore = useCallback(() => {
     void fetchNextPage()
