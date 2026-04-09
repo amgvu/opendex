f3f669ac19545b3c8426f4d6cae1ea33332d1be6
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2026-04-09 10:59:36 -0500

    fix: type improvements

--- src/lib/pokemon.ts
+++ src/lib/pokemon.ts
@@ -26,28 +26,26 @@ export const getTypeColor = (type: string) => {
 export const formatPokedexId = (id: number) =>
   `#${String(id).padStart(4, '0')}`
 
-// Attacking type → defending type → multiplier
-// Row = attacking type, Col = defending type
-// 0 = immune, 0.5 = not very effective, 2 = super effective
+// Attacking type → defending type → multiplier (omitted = 1)
 const TYPE_CHART: Record<string, Record<string, number>> = {
-  Bug:      { Bug: 1, Dark: 2, Dragon: 1, Electric: 1, Fairy: 0.5, Fighting: 0.5, Fire: 0.5, Flying: 0.5, Ghost: 0.5, Grass: 2, Ground: 1, Ice: 1, Normal: 1, Poison: 0.5, Psychic: 2, Rock: 1, Steel: 0.5, Water: 1 },
-  Dark:     { Bug: 1, Dark: 0.5, Dragon: 1, Electric: 1, Fairy: 0.5, Fighting: 0.5, Fire: 1, Flying: 1, Ghost: 2, Grass: 1, Ground: 1, Ice: 1, Normal: 1, Poison: 1, Psychic: 2, Rock: 1, Steel: 0.5, Water: 1 },
-  Dragon:   { Bug: 1, Dark: 1, Dragon: 2, Electric: 1, Fairy: 0, Fighting: 1, Fire: 1, Flying: 1, Ghost: 1, Grass: 1, Ground: 1, Ice: 1, Normal: 1, Poison: 1, Psychic: 1, Rock: 1, Steel: 0.5, Water: 1 },
-  Electric: { Bug: 1, Dark: 1, Dragon: 0.5, Electric: 0.5, Fairy: 1, Fighting: 1, Fire: 1, Flying: 2, Ghost: 1, Grass: 0.5, Ground: 0, Ice: 1, Normal: 1, Poison: 1, Psychic: 1, Rock: 1, Steel: 1, Water: 2 },
-  Fairy:    { Bug: 1, Dark: 2, Dragon: 2, Electric: 1, Fairy: 1, Fighting: 2, Fire: 0.5, Flying: 1, Ghost: 1, Grass: 1, Ground: 1, Ice: 1, Normal: 1, Poison: 0.5, Psychic: 1, Rock: 1, Steel: 0.5, Water: 1 },
-  Fighting: { Bug: 0.5, Dark: 2, Dragon: 1, Electric: 1, Fairy: 0.5, Fighting: 1, Fire: 1, Flying: 0.5, Ghost: 0, Grass: 1, Ground: 1, Ice: 2, Normal: 2, Poison: 0.5, Psychic: 0.5, Rock: 2, Steel: 2, Water: 1 },
-  Fire:     { Bug: 2, Dark: 1, Dragon: 0.5, Electric: 1, Fairy: 1, Fighting: 1, Fire: 0.5, Flying: 1, Ghost: 1, Grass: 2, Ground: 1, Ice: 2, Normal: 1, Poison: 1, Psychic: 1, Rock: 0.5, Steel: 2, Water: 0.5 },
-  Flying:   { Bug: 2, Dark: 1, Dragon: 1, Electric: 0.5, Fairy: 1, Fighting: 2, Fire: 1, Flying: 1, Ghost: 1, Grass: 2, Ground: 1, Ice: 1, Normal: 1, Poison: 1, Psychic: 1, Rock: 0.5, Steel: 0.5, Water: 1 },
-  Ghost:    { Bug: 1, Dark: 0.5, Dragon: 1, Electric: 1, Fairy: 1, Fighting: 1, Fire: 1, Flying: 1, Ghost: 2, Grass: 1, Ground: 1, Ice: 1, Normal: 0, Poison: 1, Psychic: 2, Rock: 1, Steel: 1, Water: 1 },
-  Grass:    { Bug: 0.5, Dark: 1, Dragon: 0.5, Electric: 1, Fairy: 1, Fighting: 1, Fire: 0.5, Flying: 0.5, Ghost: 1, Grass: 0.5, Ground: 2, Ice: 1, Normal: 1, Poison: 0.5, Psychic: 1, Rock: 2, Steel: 0.5, Water: 2 },
-  Ground:   { Bug: 0.5, Dark: 1, Dragon: 1, Electric: 2, Fairy: 1, Fighting: 1, Fire: 2, Flying: 0, Ghost: 1, Grass: 0.5, Ground: 1, Ice: 1, Normal: 1, Poison: 2, Psychic: 1, Rock: 2, Steel: 2, Water: 1 },
-  Ice:      { Bug: 1, Dark: 1, Dragon: 2, Electric: 1, Fairy: 1, Fighting: 1, Fire: 0.5, Flying: 2, Ghost: 1, Grass: 2, Ground: 2, Ice: 0.5, Normal: 1, Poison: 1, Psychic: 1, Rock: 1, Steel: 0.5, Water: 0.5 },
-  Normal:   { Bug: 1, Dark: 1, Dragon: 1, Electric: 1, Fairy: 1, Fighting: 1, Fire: 1, Flying: 1, Ghost: 0, Grass: 1, Ground: 1, Ice: 1, Normal: 1, Poison: 1, Psychic: 1, Rock: 0.5, Steel: 0.5, Water: 1 },
-  Poison:   { Bug: 1, Dark: 1, Dragon: 1, Electric: 1, Fairy: 2, Fighting: 1, Fire: 1, Flying: 1, Ghost: 0.5, Grass: 2, Ground: 0.5, Ice: 1, Normal: 1, Poison: 0.5, Psychic: 1, Rock: 0.5, Steel: 0, Water: 1 },
-  Psychic:  { Bug: 1, Dark: 0, Dragon: 1, Electric: 1, Fairy: 1, Fighting: 2, Fire: 1, Flying: 1, Ghost: 1, Grass: 1, Ground: 1, Ice: 1, Normal: 1, Poison: 2, Psychic: 0.5, Rock: 1, Steel: 0.5, Water: 1 },
-  Rock:     { Bug: 2, Dark: 1, Dragon: 1, Electric: 1, Fairy: 1, Fighting: 0.5, Fire: 2, Flying: 2, Ghost: 1, Grass: 1, Ground: 0.5, Ice: 2, Normal: 1, Poison: 1, Psychic: 1, Rock: 1, Steel: 0.5, Water: 1 },
-  Steel:    { Bug: 1, Dark: 1, Dragon: 1, Electric: 0.5, Fairy: 2, Fighting: 1, Fire: 0.5, Flying: 1, Ghost: 1, Grass: 1, Ground: 1, Ice: 2, Normal: 1, Poison: 1, Psychic: 1, Rock: 2, Steel: 0.5, Water: 0.5 },
-  Water:    { Bug: 1, Dark: 1, Dragon: 0.5, Electric: 1, Fairy: 1, Fighting: 1, Fire: 2, Flying: 1, Ghost: 1, Grass: 0.5, Ground: 2, Ice: 1, Normal: 1, Poison: 1, Psychic: 1, Rock: 2, Steel: 1, Water: 0.5 }
+  Bug:      { Dark: 2, Fairy: 0.5, Fighting: 0.5, Fire: 0.5, Flying: 0.5, Ghost: 0.5, Grass: 2, Poison: 0.5, Psychic: 2, Steel: 0.5 },
+  Dark:     { Dark: 0.5, Fairy: 0.5, Fighting: 0.5, Ghost: 2, Psychic: 2, Steel: 0.5 },
+  Dragon:   { Dragon: 2, Fairy: 0, Steel: 0.5 },
+  Electric: { Dragon: 0.5, Electric: 0.5, Flying: 2, Grass: 0.5, Ground: 0, Water: 2 },
+  Fairy:    { Dark: 2, Dragon: 2, Fighting: 2, Fire: 0.5, Poison: 0.5, Steel: 0.5 },
+  Fighting: { Bug: 0.5, Dark: 2, Fairy: 0.5, Flying: 0.5, Ghost: 0, Ice: 2, Normal: 2, Poison: 0.5, Psychic: 0.5, Rock: 2, Steel: 2 },
+  Fire:     { Bug: 2, Dragon: 0.5, Fire: 0.5, Grass: 2, Ice: 2, Rock: 0.5, Steel: 2, Water: 0.5 },
+  Flying:   { Bug: 2, Electric: 0.5, Fighting: 2, Grass: 2, Rock: 0.5, Steel: 0.5 },
+  Ghost:    { Dark: 0.5, Ghost: 2, Normal: 0, Psychic: 2 },
+  Grass:    { Bug: 0.5, Dragon: 0.5, Fire: 0.5, Flying: 0.5, Grass: 0.5, Ground: 2, Poison: 0.5, Rock: 2, Steel: 0.5, Water: 2 },
+  Ground:   { Bug: 0.5, Electric: 2, Fire: 2, Flying: 0, Grass: 0.5, Poison: 2, Rock: 2, Steel: 2 },
+  Ice:      { Dragon: 2, Fire: 0.5, Flying: 2, Grass: 2, Ground: 2, Ice: 0.5, Steel: 0.5, Water: 0.5 },
+  Normal:   { Ghost: 0, Rock: 0.5, Steel: 0.5 },
+  Poison:   { Fairy: 2, Ghost: 0.5, Grass: 2, Ground: 0.5, Poison: 0.5, Rock: 0.5, Steel: 0 },
+  Psychic:  { Dark: 0, Fighting: 2, Poison: 2, Psychic: 0.5, Steel: 0.5 },
+  Rock:     { Bug: 2, Fighting: 0.5, Fire: 2, Flying: 2, Ground: 0.5, Ice: 2, Steel: 0.5 },
+  Steel:    { Electric: 0.5, Fairy: 2, Fire: 0.5, Ice: 2, Rock: 2, Steel: 0.5, Water: 0.5 },
+  Water:    { Dragon: 0.5, Fire: 2, Grass: 0.5, Ground: 2, Rock: 2, Water: 0.5 }
 }
 
 const ALL_TYPES = Object.keys(TYPE_CHART)
