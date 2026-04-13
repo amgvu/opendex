9da0356639bae29fe4a2853c7d9b553e5810d3cb
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2026-04-13 04:05:27 -0500

    fix: removed scrollToRow feature

--- src/components/pokemon/PokemonGrid.tsx
+++ src/components/pokemon/PokemonGrid.tsx
@@ -67,7 +67,6 @@ export default function PokemonGrid() {
   const { columns, getRowPokemon, measureElement, totalHeight, virtualItems } =
     useVirtualGrid(
       pokemon,
-      selectedId,
       loadMore,
       hasNextPage,
       isFetchingNextPage
