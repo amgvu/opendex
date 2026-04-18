954507bb15a14cf9a7daac6fd39bf1ebde0f7bd5
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2026-04-18 05:53:49 -0500

    Revert "feat: add back url param on card"
    This reverts commit 8f9e3a64e54699a584ab2da656fc1d5f77ec1790.

--- src/hooks/filters/useSelectedPokemon.ts
+++ src/hooks/filters/useSelectedPokemon.ts
@@ -1,31 +1,15 @@
 import { useSearchParams } from 'next/navigation'
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 
 import { useSelectionStore } from '@/stores/selectionStore'
 
-import { useUrlSync } from './useUrlSync'
-
 export function useSelectedPokemon() {
   const searchParams = useSearchParams()
-  const { routerRef, searchParamsRef } = useUrlSync()
   const setSelectedNameFromUrl = useSelectionStore(s => s.setSelectedNameFromUrl)
 
-  const initialized = useRef(false)
-  if (!initialized.current) {
-    initialized.current = true
+  useEffect(() => {
     const name = searchParams.get('pokemon')
     if (name) setSelectedNameFromUrl(name)
-  }
-
-  const selectedName = useSelectionStore(s => s.selectedName)
-
-  useEffect(() => {
-    const params = new URLSearchParams(searchParamsRef.current.toString())
-    if (selectedName) {
-      params.set('pokemon', selectedName)
-    } else {
-      params.delete('pokemon')
-    }
-    routerRef.current.replace(`?${params.toString()}`, { scroll: false })
-  }, [selectedName, routerRef, searchParamsRef])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 }
