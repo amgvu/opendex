93b98c100b545ff3d0c10b4f672692123a5d8778
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2026-04-19 00:30:21 -0500

    fix: static asset cache headers

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
+    minimumCacheTTL: 31536000,
     remotePatterns: [
       {
         hostname: 'picsum.photos',
@@ -18,6 +19,37 @@ const nextConfig: NextConfig = {
       {
         hostname: 'play.pokemonshowdown.com',
         protocol: 'https'
+      }
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/icons/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
+          }
+        ]
+      },
+      {
+        source: '/(:path*\\.png)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
+          }
+        ]
+      },
+      {
+        source: '/(:path*\\.svg)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
+          }
+        ]
       }
     ]
   }
