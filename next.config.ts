7638715713a303241ba10ef7e0b9fd05d36f79c4
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2026-04-25 07:00:54 -0500

    chore: remove unused githubuser and loremflikr patterns

--- next.config.ts
+++ next.config.ts
@@ -15,18 +15,6 @@ const nextConfig: NextConfig = {
     minimumCacheTTL: 31536000,
     remotePatterns: [
       {
-        hostname: 'picsum.photos',
-        protocol: 'https'
-      },
-      {
-        hostname: 'loremflickr.com',
-        protocol: 'https'
-      },
-      {
-        hostname: 'raw.githubusercontent.com',
-        protocol: 'https'
-      },
-      {
         hostname: 'play.pokemonshowdown.com',
         protocol: 'https'
       }
