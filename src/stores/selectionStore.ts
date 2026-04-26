b88504724180c2152c8a321daac461d545d7cde4
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2026-04-26 13:41:07 -0500

    feat: update selectionstore with slidedirection, navigation, etc

--- src/stores/selectionStore.ts
+++ src/stores/selectionStore.ts
@@ -2,14 +2,20 @@ import { create } from 'zustand'
 
 type SelectionStore = {
   fromUrl: boolean
+  mode: 'flip' | 'slide'
   selectedName: null | string
+  slideDirection: 'left' | 'right' | null
+  navigateTo: (name: string, direction: 'left' | 'right') => void
   setSelectedName: (name: null | string) => void
   setSelectedNameFromUrl: (name: null | string) => void
 }
 
 export const useSelectionStore = create<SelectionStore>(set => ({
   fromUrl: false,
+  mode: 'flip',
   selectedName: null,
-  setSelectedName: name => set({ fromUrl: false, selectedName: name }),
-  setSelectedNameFromUrl: name => set({ fromUrl: name !== null, selectedName: name })
+  slideDirection: null,
+  navigateTo: (name, direction) => set({ mode: 'slide', selectedName: name, slideDirection: direction }),
+  setSelectedName: name => set({ fromUrl: false, mode: 'flip', selectedName: name, slideDirection: null }),
+  setSelectedNameFromUrl: name => set({ fromUrl: name !== null, mode: name ? 'slide' : 'flip', selectedName: name, slideDirection: null })
 }))
